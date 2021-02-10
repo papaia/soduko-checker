d54ecb58d7b583ed6f556402a92c32841c837b3a
Papaia <43409674+ItsPapaia@users.noreply.github.com>
2021-02-10 13:48:00 +0200

    style(.c): s p a c i n g

--- src/soduko_checker.c
+++ src/soduko_checker.c
@@ -16,9 +16,9 @@
 int main()
 {
     printOpenMessageForSodokoSize();
-    int n = scan_n(), bool_count=0;
+    int n = scan_n(), bool_count = 0;
     printOpenMessageForSodokoSolution();
-    int sodoko[MAX_SIZE][MAX_SIZE] = {{0}};
+    int sodoko[MAX_SIZE][MAX_SIZE] = { { 0 } };
     scan_sodoko(sodoko, n);
     bool_count += check_cols(sodoko, n);
     bool_count += check_rows(sodoko, n);
@@ -34,11 +34,11 @@ int main()
 int scan_n()
 {
     int n=0;
-    int is_true = (n>0 && (root(n) != -1));
+    int is_true = (n > 0 && (root(n) != -1));
     do
     {
         scanf("%d", &n);
-        is_true = (n>0 && (root(n) != -1));
+        is_true = (n > 0 && (root(n) != -1));
         if (is_true) return n;
     } while (!is_true);
     return n;
@@ -49,7 +49,7 @@ int scan_n()
 int calculate_sumof_nums_smaller_than_n(int n)
 {
     int sum = 0;
-    for (int i=1;i < 1+n; i++)
+    for (int i = 1; i <= n; i++)
     {
         sum += i;
     }
@@ -61,10 +61,10 @@ int check_cols(int sodoko[MAX_SIZE][MAX_SIZE], int n)
     int nums_sum = calculate_sumof_nums_smaller_than_n(n);
     int sum = 0;
     int counter = 0;
-    for (int i=0; i<n;i++)
+    for (int i = 0; i < n; i++)
     {
         sum = 0;
-        for (int j=0;j <n ; j++)
+        for (int j = 0; j <n; j++)
         {
             sum += sodoko[i][j];
         }
@@ -80,10 +80,10 @@ int check_rows(int sodoko[MAX_SIZE][MAX_SIZE], int n)
     int nums_sum = calculate_sumof_nums_smaller_than_n(n);
     int sum = 0;
     int counter = 0;
-    for (int j=0; j<n;j++)
+    for (int j = 0; j < n; j++)
     {
         sum = 0;
-        for (int i=0;i <n ; i++)
+        for (int i = 0; i < n; i++)
         {
             sum += sodoko[i][j];
         }
@@ -98,8 +98,8 @@ int check_rows(int sodoko[MAX_SIZE][MAX_SIZE], int n)
 
 int check_all_squares(int sodoko[MAX_SIZE][MAX_SIZE], int n)
 {
-    int i=0, j=0,bool_counter=0, sqrt = root(n);
-    for(int counter = 0;(j < n)&&(counter<n);counter++)
+    int i = 0, j = 0, bool_counter = 0, sqrt = root(n);
+    for(int counter = 0; (j < n) && (counter < n); counter++)
     {
         bool_counter += check_square(sodoko, i, j, n);
         i += sqrt;
@@ -121,10 +121,10 @@ int check_square(int sodoko[MAX_SIZE][MAX_SIZE],int i,int j, int n)
     int sqrt = root(n);
     int sum = 0;
     int Itmp = i, Jtmp = j;
-    for (;i<sqrt+Itmp;i++)
+    for (; i < sqrt + Itmp; i++)
     {
         j = Jtmp;
-        for (;j<sqrt+Jtmp;j++)
+        for (; j < sqrt + Jtmp; j++)
         {
             int curr = sodoko[i][j];
             sum += curr;
@@ -134,11 +134,11 @@ int check_square(int sodoko[MAX_SIZE][MAX_SIZE],int i,int j, int n)
 }
 
 
-void scan_sodoko(int sodoko[MAX_SIZE][MAX_SIZE],int n)
+void scan_sodoko(int sodoko[MAX_SIZE][MAX_SIZE], int n)
 {
-    for (int i=0;i < n;i++)
+    for (int i = 0;i < n;i++)
     {
-        for (int j=0; j < n; j++)
+        for (int j = 0; j < n; j++)
         {
             scanf("%d", &sodoko[i][j]);
         }
@@ -149,9 +149,9 @@ int root(int num)
 {
     if (num == 1) return 1;
     if (num < 0) return -1;
-    for(int i=2; i< num; i++)
+    for (int i = 2; i < num; i++)
     {
-        if (i*i == num)    return i;
+        if (i * i == num) return i;
     }
     return -1; // if no int root for number, returns -1 , used for checks
 }
